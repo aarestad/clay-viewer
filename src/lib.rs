9b1b47e1f6c04318662131df44bc9434dda7c10f
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-08-28 14:33:54 +0700

    refactored, no more loop inside

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,56 @@
 mod motion;
 
+#[macro_use]
+extern crate rental; 
+
 use std::{
     time::{Duration, Instant},
 };
 use sdl2::{
     self,
-    Sdl, VideoSubsystem, EventPump,
-    render::{WindowCanvas, TextureAccess, Texture},
+    Sdl, EventPump,
+    render::{WindowCanvas, TextureAccess},
     pixels::PixelFormatEnum,
     event::Event,
     keyboard::Keycode,
 };
 use nalgebra::{Vector3, Rotation3};
-use clay_core::{
-    Context, IdentityFilter,
-    Postproc, Renderer
-};
+use clay_core::{Image};
+
 use motion::Motion;
+
+rental! { mod rent {
+    use sdl2::{
+        video::{WindowContext},
+        render::{TextureCreator, Texture},
+    };
+
+    #[rental_mut]
+    pub struct RentTexture {
+        creator: Box<TextureCreator<WindowContext>>,
+        texture: Texture<'creator>,
+    }
+}}
+use rent::{RentTexture};
+
 
 pub struct Window {
     context: Sdl,
-    video: VideoSubsystem,
     size: (usize, usize),
     canvas: WindowCanvas,
-    capture: bool,
 
-    texture: Texture,
-    event_pump: EventPump,
+    texture: Option<RentTexture>,
+    event_pump: Option<EventPump>,
+    motion: Motion,
     state: State,
 }
 
 struct State {
-    motion: Motion,
+    capture: bool,
     drop_mouse: bool,
+    // time measurement
     instant: Instant,
     prev: Duration,
-    fps: f64,
-    printed: Duration,
 }
 
 impl Window {
@@ -52,18 +66,26 @@ impl Window {
 
         context.mouse().set_relative_mouse_mode(true);
 
-        let texture_creator = self.canvas.texture_creator();
-        let texture = texture_creator.create_texture(
-            PixelFormatEnum::RGB24,
-            TextureAccess::Streaming,
-            size.0 as u32,
-            size.1 as u32,
-        ).map_err(|e| e.to_string())?;
+        let texture_creator = canvas.texture_creator();
+        let texture = Some(RentTexture::try_new_or_drop(
+            Box::new(texture_creator),
+            |tc| {
+                tc.create_texture(
+                    PixelFormatEnum::RGB24,
+                    TextureAccess::Streaming,
+                    size.0 as u32,
+                    size.1 as u32,
+                ).map_err(|e| e.to_string())
+            }
+        )?);
+
+        let event_pump = Some(context.event_pump()?);
 
         let mut self_ = Self {
-            context, video,
-            size, canvas,
-            capture: false,
+            context, size, canvas,
+            texture, event_pump,
+            motion: Motion::new(),
+            state: Self::new_state()?,
         };
 
         self_.toggle_capture();
@@ -71,101 +93,96 @@ impl Window {
         Ok(self_)
     }
 
-    fn init_state() -> clay_core::Result<()> {
-        let mut motion = Motion::new();
-        let mut drop_mouse = true;
+    fn new_state() -> clay_core::Result<State> {
         let instant = Instant::now();
-        let mut prev = instant.elapsed();
-        let mut fps = -1.0;
-        let mut printed = instant.elapsed();
-
-        let mut event_pump = self.context.event_pump()?;
-    } 
-
-    fn toggle_capture(&mut self) {
-        self.capture = !self.capture;
-        self.context.mouse().set_relative_mouse_mode(self.capture);
+        Ok(State {
+            capture: false,
+            drop_mouse: true,
+            instant,
+            prev: instant.elapsed(),
+        })
     }
 
-    pub fn start<F>(&mut self, context: &Context, mut step: F) -> clay_core::Result<()>
-    where F: FnMut(&mut Renderer, Vector3<f64>, Rotation3<f64>) -> clay_core::Result<()> {
-        let mut screen = Postproc::<IdentityFilter>::builder()
-        .collect()?
-        .build(context, self.size, IdentityFilter::new())?;
+    fn toggle_capture(&mut self) {
+        self.state.capture = !self.state.capture;
+        self.context.mouse().set_relative_mouse_mode(self.state.capture);
+    }
 
-        
 
-        
+    pub fn poll(&mut self) -> clay_core::Result<bool> {
+        self.motion.updated = false;
 
-        
-        'main: loop {
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit {..} => break 'main,
-                    Event::KeyDown { keycode: Some(key), .. } => match key {
-                        Keycode::Escape => break 'main,
-                        Keycode::Tab => {
-                            self.toggle_capture();
-                            if self.capture {
-                                drop_mouse = true;
-                            }
-                        },
-                        _ => (),
+        let now = self.state.instant.elapsed();
+        self.motion.step(now - self.state.prev);
+        self.state.prev = now;
+
+        let mut quit = false;
+        let mut event_pump = self.event_pump.take().unwrap();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => { quit = true; },
+                Event::KeyDown { keycode: Some(key), .. } => match key {
+                    Keycode::Escape => { quit = true; },
+                    Keycode::Tab => {
+                        self.toggle_capture();
+                        if self.state.capture {
+                            self.state.drop_mouse = true;
+                        }
                     },
                     _ => (),
-                }
-                motion.handle_keys(&event);
+                },
+                _ => (),
             }
-            let rms = event_pump.relative_mouse_state();
-            if self.capture {
-                if !drop_mouse {
-                    motion.handle_mouse(&rms);
-                } else {
-                    drop_mouse = false;
-                }
-            } else if event_pump.mouse_state().left() {
-                motion.handle_mouse(&rms);
-            }
-
-            if motion.updated || motion.key_mask != 0 {
-                screen.clear()?;
-                motion.updated = false;
-            }
-            render(&mut screen, motion.pos, motion.map())?;
-            let mut n_passes = 1;
-            while instant.elapsed() - prev < Duration::from_millis(20) {
-                render(&mut screen, motion.pos, motion.map())?;
-                n_passes += 1;
-            }
-            let data = screen.read()?;
-
-            texture.update(None, &data, 3*(screen.dims().0 as usize))
-            .map_err(|e| e.to_string())?;
-
-            //self.canvas.clear();
-            self.canvas.copy(&texture, None, None)?;
-            self.canvas.present();
-
-            //thread::sleep(Duration::from_millis(20));
-            
-            let now = instant.elapsed();
-
-            let dt = now - prev;
-            motion.step(dt);
-            prev = now;
-
-            let cfps = (n_passes as f64)*1e6/(dt.as_micros() as f64);
-            if fps < 0.0 {
-                fps = cfps;
-            } else {
-                fps = 0.95*fps + 0.05*cfps;
-            }
-            if (now - printed).as_secs() > 0 {
-                println!("FPS: {:.2}", fps);
-                printed = now;
-            }
+            self.motion.handle_keys(&event);
         }
 
-        Ok(())
+        let rms = event_pump.relative_mouse_state();
+        if self.state.capture {
+            if !self.state.drop_mouse {
+                self.motion.handle_mouse(&rms);
+            } else {
+                self.state.drop_mouse = false;
+            }
+        } else if event_pump.mouse_state().left() {
+            self.motion.handle_mouse(&rms);
+        }
+
+        assert!(self.event_pump.replace(event_pump).is_none());
+        Ok(quit)
+    }
+
+    pub fn was_updated(&self) -> bool {
+        self.motion.updated || self.motion.key_mask != 0
+    }
+
+    pub fn view_params(&self) -> (Vector3<f64>, Rotation3<f64>) {
+        (self.motion.pos, self.motion.map())
+    }
+
+    pub fn size(&self) -> (usize, usize) {
+        self.size
+    }
+
+    pub fn draw(&mut self, image: &Image) -> clay_core::Result<()> {
+        let mut texture = self.texture.take().unwrap();
+
+        let res = image.read()
+        .and_then(|data| {
+            texture.rent_mut(|texture| {
+                texture.update(None, &data, 3*(image.dims().0 as usize))
+            }).map_err(|e| clay_core::Error::from(e.to_string()))
+        })
+        .and_then(|()| {
+            //self.canvas.clear();
+            texture.rent(|texture| {
+                self.canvas.copy(texture, None, None)
+                .map_err(|e| clay_core::Error::from(e))
+            })
+            .map(|()| self.canvas.present())
+        });
+
+        assert!(self.texture.replace(texture).is_none());
+
+        res
     }
 } 
